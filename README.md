0175d48f01e6122640076bf6ca1dd352b3774fea
Nordine Bittich <nordine@bittich.be>
2024-02-25 13:00:37 +0100

    direct command

--- README.md
+++ README.md
@@ -2,6 +2,12 @@
 
 Simple command line tool for basic conversions.
 
+## Usage
+
+### REPL
+
+`./kofte`
+
 ```
 > help
 mile_to_km              km_to_mile              inch_to_cm
@@ -10,3 +16,13 @@ yard_to_meter           meter_to_yard           fahr_to_celcius
 celcius_to_fahr         kelvin_to_celcius       celcius_to_kelvin
 help                    exit
 ```
+
+### Command
+
+`./kofte mile_to_km 89.34`
+
+## Shortcuts
+
+First letter of each word in a command. (`mtk == mile_to_km`)
+
+e.g: `./kofte ktc 89.34`
--- kofte.c
+++ kofte.c
@@ -1,4 +1,5 @@
 #include "conversion.h"
+#include <assert.h>
 #include <ctype.h>
 #include <signal.h>
 #include <stdbool.h>
@@ -7,7 +8,7 @@
 #include <string.h>
 
 #define VERSION "1"
-
+#define ARRAY_SIZE(arr) (sizeof(arr) / sizeof(arr[0]))
 const char *AVAILABLE_COMMANDS[] = {"mile_to_km",
                                     "km_to_mile",
                                     "inch_to_cm",
@@ -22,9 +23,16 @@ const char *AVAILABLE_COMMANDS[] = {"mile_to_km",
                                     "celcius_to_kelvin",
                                     "help",
                                     "exit"};
+const char *AVAILABLE_COMMANDS_ALT[] = {"mtk", "ktm", "itc", "cti", "ftm",
+                                        "mtf", "ytm", "mty", "ftc", "ctf",
+                                        "ktc", "ctk", "h",   "e"};
 
-const size_t AVAILABLE_COMMANDS_SIZE =
-    (sizeof AVAILABLE_COMMANDS / sizeof AVAILABLE_COMMANDS[0]);
+#define AVAILABLE_COMMANDS_SIZE ARRAY_SIZE(AVAILABLE_COMMANDS)
+
+#define AVAILABLE_COMMANDS_ALT_SIZE ARRAY_SIZE(AVAILABLE_COMMANDS_ALT)
+
+static_assert(AVAILABLE_COMMANDS_ALT_SIZE - AVAILABLE_COMMANDS_SIZE == 0,
+              "ALT COMMAND SIZE != COMMAND SIZE");
 
 typedef enum {
   MILES_TO_KM,
@@ -62,6 +70,19 @@ bool is_command(const char *input, int start, int end, const char *command) {
   return strncmp(input + start, command, length) == 0;
 }
 
+int find_command(char *input, int start_cmd, int end_cmd) {
+  for (int i = 0; i < AVAILABLE_COMMANDS_SIZE; i++) {
+    if (is_command(input, start_cmd, end_cmd, AVAILABLE_COMMANDS[i])) {
+      return i;
+    }
+  }
+  for (int i = 0; i < AVAILABLE_COMMANDS_ALT_SIZE; i++) {
+    if (is_command(input, start_cmd, end_cmd, AVAILABLE_COMMANDS_ALT[i])) {
+      return i;
+    }
+  }
+  return -1;
+}
 int parse_command(char *input, command_t *output, const size_t *len) {
 
   int start_cmd = -1, end_cmd = -1;
@@ -104,24 +125,17 @@ int parse_command(char *input, command_t *output, const size_t *len) {
     end_num = start_num;
   }
 
-  int *found = NULL;
-  int i;
+  int found = find_command(input, start_cmd, end_cmd);
 
-  for (i = 0; i < AVAILABLE_COMMANDS_SIZE; i++) {
-    if (is_command(input, start_cmd, end_cmd, AVAILABLE_COMMANDS[i])) {
-      found = &i;
-      break;
-    }
-  }
+  if (found >= 0) {
+    output->command = found;
+    char *end = input + end_num + 1;
+    output->input = strtold(input + start_num, &end);
 
-  if (found) {
-    output->command = *found;
   } else {
     printf("COMMAND NOT FOUND\n");
     return -1;
   }
-  char *end = input + end_num + 1;
-  output->input = strtold(input + start_num, &end);
   return 0;
 }
 
@@ -131,91 +145,107 @@ void clear_buffer(char *input, int size) {
   }
 }
 
-void run_command(char *input, command_t *command, const size_t *len) {
-  int res = parse_command(input, command, len);
+void run_command(command_t *command) {
 
-  if (res == 0) {
-    switch (command->command) {
-    case MILES_TO_KM:
-      printf("%.2Lf miles is equal to %.2Lf km\n", command->input,
-             miles_to_km(&command->input));
-      break;
-    case KM_TO_MILES:
-      printf("%.2Lf km is equal to %.2Lf miles\n", command->input,
-             km_to_miles(&command->input));
-      break;
-    case INCH_TO_CM:
-      printf("%.2Lf inch is equal to %.2Lf cm\n", command->input,
-             inch_to_cm(&command->input));
-      break;
-    case CM_TO_INCH:
-      printf("%.2Lf cm is equal to %.2Lf inch\n", command->input,
-             cm_to_inch(&command->input));
-      break;
-    case FOOT_TO_METER:
-      printf("%.2Lf foot is equal to %.2Lf meters\n", command->input,
-             foot_to_meter(&command->input));
-      break;
-    case METER_TO_FOOT:
-      printf("%.2Lf meters is equal to %.2Lf foot\n", command->input,
-             meter_to_foot(&command->input));
-      break;
-    case YARD_TO_METER:
-      printf("%.2Lf yards is equal to %.2Lf meters\n", command->input,
-             yard_to_meter(&command->input));
-      break;
-    case METER_TO_YARD:
-      printf("%.2Lf meters is equal to %.2Lf yards\n", command->input,
-             meter_to_yard(&command->input));
-      break;
-    case CELCIUS_TO_FAHR:
-      printf("%.2Lf°C is equal to %.2Lf°F\n", command->input,
-             celcius_to_fahrenheit(&command->input));
-      break;
-    case FAHR_TO_CELCIUS:
-      printf("%.2Lf°F is equal to %.2Lf°C\n", command->input,
-             fahrenheit_to_celcius(&command->input));
-      break;
-    case CELCIUS_TO_KELVIN:
-      printf("%.2Lf°C is equal to %.2Lf°K\n", command->input,
-             celcius_to_kelvin(&command->input));
-      break;
-    case KELVIN_TO_CELCIUS:
-      printf("%.2Lf°K is equal to %.2Lf°C\n", command->input,
-             kelvin_to_celcius(&command->input));
-      break;
-    case EXIT: {
-      printf("BYE\n");
-      exit(0);
-    }
-    case HELP: {
-      int max_len = strlen(AVAILABLE_COMMANDS[0]);
-      for (size_t i = 1; i < AVAILABLE_COMMANDS_SIZE; ++i) {
-        int len = strlen(AVAILABLE_COMMANDS[i]);
-        if (len > max_len) {
-          max_len = len;
-        }
-      }
-      for (size_t i = 0; i < AVAILABLE_COMMANDS_SIZE; ++i) {
-        printf("%-*s\t", max_len, AVAILABLE_COMMANDS[i]);
-        if ((i + 1) % 3 == 0) {
-          putchar('\n');
-        }
-      }
-      putchar('\n');
-      break;
-    }
-    default:
-      printf("not implemented yet \n");
-    }
+  switch (command->command) {
+  case MILES_TO_KM:
+    printf("%.2Lf miles is equal to %.2Lf km\n", command->input,
+           miles_to_km(&command->input));
+    break;
+  case KM_TO_MILES:
+    printf("%.2Lf km is equal to %.2Lf miles\n", command->input,
+           km_to_miles(&command->input));
+    break;
+  case INCH_TO_CM:
+    printf("%.2Lf inch is equal to %.2Lf cm\n", command->input,
+           inch_to_cm(&command->input));
+    break;
+  case CM_TO_INCH:
+    printf("%.2Lf cm is equal to %.2Lf inch\n", command->input,
+           cm_to_inch(&command->input));
+    break;
+  case FOOT_TO_METER:
+    printf("%.2Lf foot is equal to %.2Lf meters\n", command->input,
+           foot_to_meter(&command->input));
+    break;
+  case METER_TO_FOOT:
+    printf("%.2Lf meters is equal to %.2Lf foot\n", command->input,
+           meter_to_foot(&command->input));
+    break;
+  case YARD_TO_METER:
+    printf("%.2Lf yards is equal to %.2Lf meters\n", command->input,
+           yard_to_meter(&command->input));
+    break;
+  case METER_TO_YARD:
+    printf("%.2Lf meters is equal to %.2Lf yards\n", command->input,
+           meter_to_yard(&command->input));
+    break;
+  case CELCIUS_TO_FAHR:
+    printf("%.2Lf°C is equal to %.2Lf°F\n", command->input,
+           celcius_to_fahrenheit(&command->input));
+    break;
+  case FAHR_TO_CELCIUS:
+    printf("%.2Lf°F is equal to %.2Lf°C\n", command->input,
+           fahrenheit_to_celcius(&command->input));
+    break;
+  case CELCIUS_TO_KELVIN:
+    printf("%.2Lf°C is equal to %.2Lf°K\n", command->input,
+           celcius_to_kelvin(&command->input));
+    break;
+  case KELVIN_TO_CELCIUS:
+    printf("%.2Lf°K is equal to %.2Lf°C\n", command->input,
+           kelvin_to_celcius(&command->input));
+    break;
+  case EXIT: {
+    printf("BYE\n");
+    exit(0);
   }
-  clear_buffer(input, *len);
+  case HELP: {
+    int max_len = strlen(AVAILABLE_COMMANDS[0]);
+    for (size_t i = 1; i < AVAILABLE_COMMANDS_SIZE; ++i) {
+      int len = strlen(AVAILABLE_COMMANDS[i]);
+      if (len > max_len) {
+        max_len = len;
+      }
+    }
+    for (size_t i = 0; i < AVAILABLE_COMMANDS_SIZE; ++i) {
+      printf("%-*s <number> \t", max_len, AVAILABLE_COMMANDS[i]);
+      if ((i + 1) % 3 == 0) {
+        putchar('\n');
+      }
+    }
+    putchar('\n');
+    break;
+  }
+  default:
+    printf("not implemented yet \n");
+  }
 }
 
-int main() {
-  signal(SIGINT, intHandler);
-  printf("Conversion REPL v%s\n", VERSION);
+int main(int argc, char **argv) {
   command_t command = {0};
+  if (argc > 1) {
+    char *command_str = argv[1];
+    int len = strlen(command_str);
+    int found = find_command(argv[1], 0, len);
+    if (found >= 0) {
+      command.command = found;
+      if (argc > 2) {
+        char *input_str = argv[2];
+        int len = strlen(input_str);
+        char *end = input_str + len + 1;
+        command.input = strtold(input_str, &end);
+      }
+      run_command(&command);
+      return 0;
+    } else {
+      printf("COMMAND not found \n");
+      return -1;
+    }
+  }
+
+  signal(SIGINT, intHandler);
+  printf("Köfte v%s\n", VERSION);
   char *input = NULL;
   size_t len = 0;
   ssize_t read = 0;
@@ -227,6 +257,11 @@ int main() {
       printf("\n");
       continue;
     }
-    run_command(input, &command, &len);
+
+    int res = parse_command(input, &command, &len);
+    if (res == 0) {
+      run_command(&command);
+      clear_buffer(input, len);
+    }
   }
 }
